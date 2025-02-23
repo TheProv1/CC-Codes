0f00a961d44ef3cf072a4099e5db8e6888157bb0
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2025-02-23 21:08:50 +0400

    Update runcodes.sh

--- Lab3/runcodes.sh
+++ Lab3/runcodes.sh
@@ -4,17 +4,17 @@ lex code3.l && cc lex.yy.c -ll -o code3
 lex code4.l && cc lex.yy.c -ll -o code4 
 
 echo "Code 1: Begin"
-./code1
+./code1 || code1
 echo "Code 1: End"
 
 echo "Code 2: Begin"
-./code2
+./code2 || code2
 echo "Code 2: End"
 
 echo "Code 3: Begin"
-./code3
+./code3 || code3
 echo "Code 3: End"
 
 echo "Code 4: Begin"
-./code4
+./code4 || code4
 echo "Code 4: End"
