b974abbb044f4fdaf24d00c0eb7e684f366c7e38
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2025-02-23 11:18:12 +0400

    Update and rename read.sh to runcodes.sh

--- Lab3/read.sh
+++
@@ -1,4 +0,0 @@
-lex code1.l && cc lex.yy.c -ll -o code1 && ./code1
-lex code2.l && cc lex.yy.c -ll -o code2 && ./code2
-lex code3.l && cc lex.yy.c -ll -o code3 && ./code3
-lex code4.l && cc lex.yy.c -ll -o code4 && ./code4
---
+++ Lab3/runcodes.sh
@@ -0,0 +1,20 @@
+lex code1.l && cc lex.yy.c -ll -o code1 
+lex code2.l && cc lex.yy.c -ll -o code2 
+lex code3.l && cc lex.yy.c -ll -o code3 
+lex code4.l && cc lex.yy.c -ll -o code4 
+
+echo "Code 1: Begin"
+./code1
+echo "Code 1: End"
+
+echo "Code 2: Begin"
+./code2
+echo "Code 2: End"
+
+echo "Code 3: Begin"
+./code3
+echo "Code 3: End"
+
+echo "Code 4: Begin"
+./code4
+echo "Code 4: End"
